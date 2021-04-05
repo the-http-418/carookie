9515d2c18c0641d3cd4711cf675c35a0d9d341be
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-04-05 09:50:21 +0530

    Added wishes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="hello">
-    <img id="caropic" alt="caro" src="https://techwarts.tech/assets/img/IMG_1061%20Cropped.jpg"/>
+    <img
+      id="caropic"
+      alt="caro"
+      src="https://techwarts.tech/assets/img/IMG_1061%20Cropped.jpg"
+    />
     <h1>Hey! I'm Caro, but you can call me CJ.</h1>
-    <h2>
-      I make amazing cookies!
-    </h2>
-    <h3>
-      Hit me up for some ooey-gooey chocolatey amazingness!
-    </h3>
+    <h2>And it's my birthday today!!!!!</h2>
+    <h3>Click here to wish me a happy birthday</h3>
+    <form>
+      <input type="text" id="name" value="name" placeholder="Your Name" />
+      <button v-on:click="wish">Wish</button>
+    </form>
   </div>
 </template>
 
 <script>
+const axios = require("axios");
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    wish: function (event) {
+      console.log("HIIII");
+      event.preventDefault();
+      var content = "Dear Caroline, ";
+      content += document.getElementById("name").value;
+      content += " wishes you a very Happy Birthday! 🎊";
+      let data = { content: content };
+      axios.post("https://discord.com/api/webhooks/828480703288705055/ndqcUqphtUUpGTs1usNlvmgKt8URDe2v1IHZlQvW-lW-hDmRGJ8m6Gb5osBufg1Wt3yt", data);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -26,7 +43,7 @@ export default {
   background-image: url'(https://images.unsplash.com/photo-1499636136210-6f4ee915583e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80');
 } */
 #caropic {
-  max-width: 50%;
+  max-width: 30%;
 }
 h3 {
   margin: 40px 0 0;
